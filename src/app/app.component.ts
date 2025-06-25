c036fbd2ae7ecb24e599abf33b56652d38718172
zahid94 <zahidulislam5594@gmail.com>
2025-06-25 15:08:50 +0600

    angular with kendo ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import { KENDO_DATEINPUTS } from '@progress/kendo-angular-dateinputs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [KENDO_DATEINPUTS],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
